e7a290bbbe7432c8e95b4479a3b19713c95520b1
Juggy <alexandrujuganaru128@gmail.com>
2026-09-25 01:59:57 +0300

    Most of the needed backend functions added

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import Depends, FastAPI
 from models import User, Meal
 from database import session, engine
 import database_models
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import date
 database_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -41,6 +43,12 @@ def get_user(id: int,db: Session = Depends(get_db)):
     return "Not found"
 
 
+@app.get("/meals/{user_id}/{logged_in}")
+def get_user_meals(user_id: int,logged_in: date ,db: Session = Depends(get_db)):
+    db_user=s=db.query(database_models.Meal).filter(database_models.Meal.user_id==user_id ,database_models.Meal.logged_in==logged_in).all()
+    if db_user:
+        return db_user
+    return "Not found"
 
 @app.post("/users/")
 def add_user(user: User,db: Session = Depends(get_db)):
@@ -49,7 +57,39 @@ def add_user(user: User,db: Session = Depends(get_db)):
     return user
 
 @app.post("/meals/")
-def add_user(meal: Meal,db: Session = Depends(get_db)):
+def add_meal(meal: Meal,db: Session = Depends(get_db)):
     db.add(database_models.Meal(**meal.model_dump()))
     db.commit()
     return meal
+
+@app.put("/meals/{id}")
+def update_meal(id: int, meal:Meal,db: Session = Depends(get_db)):
+    db_meal = db.query(database_models.Meal).filter(database_models.Meal.id==id).first()
+    user_exists = db.query(database_models.User).filter(database_models.User.id == meal.user_id).first()
+    if db_meal and user_exists:
+        db_meal.user_id = meal.user_id
+        db_meal.name = meal.name
+        db_meal.calories = meal.calories
+        db_meal.logged_in = meal.logged_in
+        db.commit()
+        return "Product updated"
+    return "Not found"
+
+@app.delete("/meals/{id}")
+def delete_meal(id: int, db: Session = Depends(get_db)):
+    db_meal = db.query(database_models.Meal).filter(database_models.Meal.id==id).first()
+    if db_meal:
+        db.delete(db_meal)
+        db.commit()
+        return "Deleted succesfully"
+    return "Not found"
+    
+@app.get("/users/{user_id}/summary")
+def summary(user_id: int,db: Session = Depends(get_db)):
+    total_calories = db.query(func.sum(database_models.Meal.calories)).filter(database_models.Meal.user_id==user_id).scalar()
+    total_calories = total_calories or 0
+    return {"total_calories":total_calories}
+
+
+
+
